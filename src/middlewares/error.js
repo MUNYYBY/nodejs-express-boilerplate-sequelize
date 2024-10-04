8aa21f0d9ee7262602d7469cbc8b26855656f45d
MUNYYBY <hm.muneeb.u.r@gmail.com>
2024-10-04 13:19:40 +0500

    chore: removed docker, github actions and global error summerizer

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -18,10 +18,10 @@ const errorConverter = (err, req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
-  if (config.env === 'production' && !err.isOperational) {
-    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
-  }
+  // if (config.env === 'production' && !err.isOperational) {
+  //   statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  //   message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+  // }
 
   res.locals.errorMessage = err.message;
 
